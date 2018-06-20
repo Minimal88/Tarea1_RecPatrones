46485546300f4fa9b2a8b02d34a737c296c00146
Unknown <tean88@gmail.com>
2018-06-20 07:37:25 -0600

    reacomodo

--- poverty.py
+++ poverty.py
@@ -1,23 +1,31 @@
 import pandas as pd
-
-url = "C:/Users/gollo/gitRepos/Tarea1_RecPatrones/data/MPI_national.csv"
-# load dataset into Pandas DataFrame
-df = pd.read_csv(url, names=['ISO','Country','MPI Urban','Headcount Ratio Urban','Intensity of Deprivation Urban','MPI Rural','Headcount Ratio Rural','Intensity of Deprivation Rural'])
-
-
-
+import matplotlib
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+
+url = "C:/Users/gollo/gitRepos/Tarea1_RecPatrones/data/MPI_national_labeled.csv"
+# load dataset into Pandas DataFrame
+df = pd.read_csv(url, names=['ISO','Country','MPI Urban','Headcount Ratio Urban','Intensity of Deprivation Urban','MPI Rural','Headcount Ratio Rural','Intensity of Deprivation Rural','Nivel'])
+
+
+#print(df)
+
 features = ['MPI Urban','Headcount Ratio Urban','Intensity of Deprivation Urban','MPI Rural','Headcount Ratio Rural','Intensity of Deprivation Rural']
 # Separating out the features
 x = df.loc[:, features].values
+
+
 # Separating out the target
-y = df.loc[:,['ISO']].values
+y = df.loc[:,['Nivel']].values
 
 # Standardizing the features
 x = StandardScaler().fit_transform(x)
 
+#print(y)
 
-from sklearn.decomposition import PCA
+
 pca = PCA(n_components=2)
 principalComponents = pca.fit_transform(x)
 principalDf = pd.DataFrame(data = principalComponents
@@ -25,29 +33,31 @@ principalDf = pd.DataFrame(data = principalComponents
 
 
 
-finalDf = pd.concat([principalDf, df[['MPI Urban']]], axis = 1)
+finalDf = pd.concat([principalDf, df[['Nivel']]], axis = 1)
 
 
 
-import matplotlib
-import matplotlib.pyplot as plt
 
 fig = plt.figure(figsize = (8,8))
 ax = fig.add_subplot(1,1,1) 
-ax.set_xlabel('Principal Component 1', fontsize = 15)
-ax.set_ylabel('Principal Component 2', fontsize = 15)
-ax.set_title('2 component PCA', fontsize = 20)
-targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+ax.set_xlabel('PCA 1', fontsize = 15)
+ax.set_ylabel('PCA 2', fontsize = 15)
+ax.set_title('2 componenentes de PCA', fontsize = 20)
+countries = ['Nivel_1', 'Nivel_2', 'Nivel_3']
 colors = ['r', 'g', 'b']
-for target, color in zip(targets,colors):
-    indicesToKeep = finalDf['target'] == target
+for country, color in zip(countries,colors):
+    indicesToKeep = finalDf['Nivel'] == country
     ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
                , finalDf.loc[indicesToKeep, 'principal component 2']
                , c = color
                , s = 50)
-ax.legend(targets)
+ax.legend(countries)
 ax.grid()
 
+
+
+
+plt.show()
 
 
 pca.explained_variance_ratio_
